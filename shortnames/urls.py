a0719acc6cba941bd879e019767687b146fed212
Tim Arbuckle <tim.arbuckle@openx.com>
2015-03-31 15:29:05 -0700

    include sample json files to demo shortened key names

--- shortnames/urls.py
+++ shortnames/urls.py
@@ -5,4 +5,6 @@ from shortnames import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'json/$', views.show_json_keynames, name='json'),
+    url(r'demos/$', views.demos, name='demos'),
+    url(r'demos/(?P<filename>\w+)/$', views.demo_file, name='demo_file'),
 )
--- shortnames/views.py
+++ shortnames/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
+from django.conf import settings
 from .forms import UploadFileForm
 import json
 
@@ -9,6 +10,35 @@ from .models import Attribute
 
 def index(request):
     return HttpResponse('nothing here, move along')
+
+
+def _link(filename):
+    return {'link': filename, 'name': filename.split('.')[0]}
+
+
+def demos(request):
+    data = [
+        _link("expandable_creative.json"),
+        _link("mobile.json"),
+        _link("native_ad.json"),
+        _link("pmp_direct_deal.json"),
+        _link("simple_banner.json"),
+        _link("video.json")
+    ]
+    context = {'data': data}
+    return render(request, 'demos.html', context)
+
+
+def demo_file(request, filename):
+    fullpathname = '%s/tests/%s.json' % (settings.BASE_DIR, filename)
+    f = open(fullpathname, 'r')
+    json_data = json.loads(f.read())
+    f.close()
+    jp = JsonPath()
+    jpdata = jp.convert(json_data)
+    data = _convert_to_short(jpdata)
+    context = {'data': data}
+    return render(request, 'keynames.html', context)
 
 
 def show_json_keynames(request):
