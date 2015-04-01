bfd59864a2eef902bbf17c31e3d59313dac33dad
Tim Arbuckle <timarbuckle@gmail.com>
2015-04-01 01:10:20 -0400

    tweak urls so can serve from nginx on personal host

--- ortb/urls.py
+++ ortb/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ortb.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns('',
     url(r'^ortb/', include('shortnames.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^ortb/admin/', include(admin.site.urls)),
 )
